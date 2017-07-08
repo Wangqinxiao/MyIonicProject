27a271e67487ef6878793017bea4381ca1fedcd6
Wangqinxiao <13319255075@163.com>
2017-07-08 19:13:12 +0800

    main page and words index page

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { WordsPage } from '../words/words';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
+  }
+  itemTapped(event, item) {
+    this.navCtrl.push(WordsPage, {
+      item: item
+    });
   }
 }
---
+++ src/pages/words/words.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-words',
+  templateUrl: 'words.html'
+})
+export class WordsPage {
+  type: string = 'overUsedWords';
+  constructor() {
+    
+  }
+}
